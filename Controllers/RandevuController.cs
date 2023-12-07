44201110fce1c66d37534f9fc89e415d088339cb
osmanzekiy <104172490+osmanzekiy@users.noreply.github.com>
2023-12-08 01:13:15 +0300

    Randevu
    Randevu Ekranı ve Controllerı Eklendi.Migrationlar Eklendi

---
+++ Controllers/RandevuController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using System.Text;
+using WebProje.Models;
+
+namespace WebProje.Controllers
+{
+    [Authorize(Roles ="Hasta,Doktor,Admin")]
+    public class RandevuController : Controller
+    {
+        private readonly HastaneContext _context;
+        
+        public RandevuController(HastaneContext context)
+        {
+            _context = context;
+        }
+        public IActionResult RandevuAl()
+        {
+          
+          
+            var datas=_context.Bolumler.ToList();
+            ViewBag.Veriler = new SelectList(datas.Select(e => new SelectListItem
+            {
+                Value = e.BolumId.ToString(),
+                Text = e.BolumAdi
+            }), "Value", "Text");
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RandevuAl(RandevuAl randevu)
+        {
+            if (ModelState.IsValid)
+            {
+                Randevu _randevu=new Randevu();
+                //_randevu=_context.Randevular.Where(r => r.RandevuId == int.Parse(randevu.Date)).FirstOrDefault();
+                //_randevu.IsOpen = false;
+                string apiUrl = "https://localhost:7268/api/ApiRandevu/RandevuKaydet";
+                var postData = randevu;
+
+                using (HttpClient client = new HttpClient())
+                {
+                    // JSON verilerini dönüştürme
+                    string jsonData =JsonConvert.SerializeObject(postData);
+
+                    // İstek başlıklarını ayarlama (eğer gerekiyorsa)
+                    // client.DefaultRequestHeaders.Add("Authorization", "Bearer your-access-token");
+
+                    // POST isteği gönderme
+                    HttpResponseMessage response = await client.PostAsync(apiUrl, new StringContent(jsonData, Encoding.UTF8, "application/json"));
+
+                    // İstek sonucunu işleme
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Başarılı işlemler burada yapılabilir
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        ViewBag.ResponseData = responseContent;
+                    }
+                    else
+                    {
+                        // Hata durumları burada işlenebilir
+                        ViewBag.ErrorMessage = "Web API isteği başarısız oldu. Durum kodu: " + response.StatusCode;
+                    }
+                }
+                TempData["IslemBasarili"]="Randevunuz Başarıyla Oluşturuldu!!!";
+                return RedirectToAction("Anasayfa","Main");
+            }
+            else
+            {
+          
+                var datas = _context.Bolumler.ToList();
+                ViewBag.Veriler = new SelectList(datas.Select(e => new SelectListItem
+                {
+                    Value = e.BolumId.ToString(),
+                    Text = e.BolumAdi
+                }), "Value", "Text");
+                return View(randevu);
+            }
+        }
+
+        public IActionResult RandevuIptal(int RandevuId)
+        {
+            //var data = _context.Randevular.Find(RandevuId);
+            //_context.Randevular.Remove(data);
+            //_context.SaveChanges();
+            return RedirectToAction("Anasayfa","Main");
+        }
+
+
+        public JsonResult DoktorGetir(string selectedEntity)
+        {
+            if (selectedEntity != null)
+            {
+                int bolumId = int.Parse(selectedEntity);
+                var datas = _context.Doktorlar.Where(e => e.BolumId == bolumId).ToList();
+                string tcm = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                if (datas.Any(d => d.TC == User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                    datas.Remove(datas.Where(d => d.TC == User.FindFirst(ClaimTypes.NameIdentifier).Value).FirstOrDefault());
+
+                return Json(datas);
+            }
+            else return Json(null);
+        }
+        public JsonResult RandevuGetir(string selectedEntity)
+        {
+            int doktorid=int.Parse(selectedEntity);
+            var datas = _context.Randevular.Where(e => e.DoktorId == doktorid && e.IsOpen==true).Select(d => new
+            {
+                randevuid=d.RandevuId,
+                tarih=d.Tarih.ToString("dd.MM.yyyy HH:mm:ss"),
+                isOpen=d.IsOpen
+            }).ToList();
+            return Json(datas);
+        }
+    }
+}
---
+++ Migrations/20231021102354_Deneme1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebProje.Migrations
+{
+    /// <inheritdoc />
+    public partial class Deneme1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bolumler",
+                columns: table => new
+                {
+                    BolumId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BolumAdi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bolumler", x => x.BolumId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hastalar",
+                columns: table => new
+                {
+                    HastaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TC = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Isim = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyisim = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefon = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cinsiyet = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hastalar", x => x.HastaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doktorlar",
+                columns: table => new
+                {
+                    DoktorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Isim = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyisim = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BolumId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doktorlar", x => x.DoktorId);
+                    table.ForeignKey(
+                        name: "FK_Doktorlar_Bolumler_BolumId",
+                        column: x => x.BolumId,
+                        principalTable: "Bolumler",
+                        principalColumn: "BolumId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doktorlar_BolumId",
+                table: "Doktorlar",
+                column: "BolumId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doktorlar");
+
+            migrationBuilder.DropTable(
+                name: "Hastalar");
+
+            migrationBuilder.DropTable(
+                name: "Bolumler");
+        }
+    }
+}
