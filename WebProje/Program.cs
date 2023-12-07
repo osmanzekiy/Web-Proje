50527e9eaf6ba3c1d29b8b4393b157d74008059f
Osman Zeki <Osman Zeki@DESKTOP-TKHK6PD>
2023-12-08 01:26:28 +0300

    Randevu Ekranı, EF ve Migrations

--- WebProje/Program.cs
+++ WebProje/Program.cs
@@ -1,7 +1,12 @@
+using WebProje.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<HastaneContext>();
+
 
 var app = builder.Build();
 
