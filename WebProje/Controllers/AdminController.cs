f6ccaa7fb5209f12100259b845a8c77115c2d988
Osman Zeki <Osman Zeki@DESKTOP-TKHK6PD>
2023-12-17 11:26:13 +0300

    Admin Girişi ve Admin İşlemleri

---
+++ WebProje/Controllers/AdminController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using WebProje.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly HastaneContext _context;
+        public AdminController(HastaneContext context)
+        {
+            _context = context;
+        }
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(AdminLogin admin)
+        {
+            if(ModelState.IsValid)
+            {
+                if(_context.Adminler.Any(p=>p.Mail==admin.Email && p.Password == admin.Password))
+                {
+                    var _admin = _context.Adminler.Include(p=>p.Rol).Where(p => p.Mail == admin.Email && p.Password == admin.Password).FirstOrDefault();
+                    List<Claim> claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, _admin.RolId.ToString()),
+                        new Claim(ClaimTypes.Role,_admin.Rol.RolAdi.ToString()),
+                        new Claim("Isim",_admin.Isim),
+                        new Claim("Soyisim",_admin.Soyisim)
+
+                    };
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var authProperties = new AuthenticationProperties
+                    {
+
+                    };
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                                                  new ClaimsPrincipal(claimsIdentity),
+                                                  authProperties);
+                    return RedirectToAction("Anasayfa", "Main");
+                }
+                else
+                {
+                    ModelState.AddModelError("Password","Kullanıcı Adı veya Şifre Hatalı!");
+                }
+                
+            }
+            return View(admin);
+        }
+        [Authorize(Roles ="Admin")]
+        public IActionResult AdminSayfa()
+        {
+            ViewBag.Bölümler= new SelectList(_context.Bolumler.Select(e => new SelectListItem
+            {
+                Value = e.BolumId.ToString(),
+                Text = e.BolumAdi
+            }), "Value", "Text");
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult RandevuEkle(RandevuEkleViewModel model)
+        {
+            if (ModelState.IsValid && _context.Doktorlar.Any(r => r.DoktorId == int.Parse(model.Doktor)))
+            {
+                List<CalismaSaatleri> doktorsaatleri = _context.CalismaSaatleri.Include(r => r.Doktor).Where(r => r.DoktorId == int.Parse(model.Doktor)).ToList();
+                var data = new[] { "Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi" };
+                var calismagunu = data[(int)(model.Tarih.DayOfWeek)];
+                foreach(var item in doktorsaatleri)
+                {
+                    if (item.Gun == calismagunu && model.Tarih.TimeOfDay>item.Baslangic && model.Tarih.TimeOfDay<item.Bitis)
+                    {
+                        Randevu randevu = new Randevu();
+                        randevu.DoktorId = int.Parse(model.Doktor);
+                        randevu.Tarih = model.Tarih;
+                        randevu.IsOpen = true;
+                        _context.Randevular.Add(randevu);
+                        _context.SaveChanges();
+                        TempData["mesaj"] = "Başarıyla Eklendi!";
+                        return RedirectToAction("AdminSayfa");
+                    }
+                }
+                ViewBag.hata = "Lütfen Çalışma Saatlerini Dikkate Alınız !";
+                
+            }
+             
+                ViewBag.Bölümler = new SelectList(_context.Bolumler.Select(e => new SelectListItem
+                {
+                    Value = e.BolumId.ToString(),
+                    Text = e.BolumAdi
+                }), "Value", "Text");
+                return View("AdminSayfa", model); 
+        }
+    }
+}
---
+++ WebProje/Controllers/LoginController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebProje.Models;
+using NuGet.Protocol.Plugins;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebProje.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly HastaneContext _context;
+
+        public LoginController(HastaneContext context)
+        {
+            _context = context;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GirisYap(Login login)
+        {
+            bool isDoktor = false;
+            if (ModelState.IsValid)
+            {
+            string tc = login.TC;
+            string password = login.Sifre;
+            List<Claim> claims;
+          
+            
+            if(_context.Doktorlar.Any(d=>d.TC==tc && d.Password==password))
+            {
+             var d=_context.Doktorlar.Include(p=>p.Rol).Where(d=>d.TC==tc && d.Password==password).FirstOrDefault();
+                     claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, tc),
+                        new Claim(ClaimTypes.Role,d.Rol.RolAdi.ToString()),
+                        new Claim("Isim",d.Isim),
+                        new Claim("Soyisim",d.Soyisim)
+                       
+                    };
+                    isDoktor=true;
+                }
+            else if(_context.Hastalar.Any(h=>h.TC==tc && h.Password==password))
+            {
+              var h=_context.Hastalar.Include(p=>p.Rol).Where(h=>h.TC==tc && h.Password==password).FirstOrDefault();
+                     claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, tc),
+                        new Claim(ClaimTypes.Role,h.Rol.RolAdi.ToString()),
+                        new Claim("Isim",h.Isim),
+                        new Claim("Soyisim",h.Soyisim)
+               
+                    };
+                    isDoktor = false;
+                }
+            else
+            
+                
+                
+                {
+                    ModelState.AddModelError("Sifre", "Kullanıcı Adı veya Şifre Hatalı !");
+                    return View();
+            }
+
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var authProperties = new AuthenticationProperties
+                    {
+                       
+                    };
+
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                                                  new ClaimsPrincipal(claimsIdentity),
+                                                  authProperties);
+                if (isDoktor)
+                {
+                    return RedirectToAction("Anasayfa", "Doktor");
+                }
+
+                    return RedirectToAction("Anasayfa", "Main");
+                
+               
+            }
+            else
+            {
+                ModelState.AddModelError("Sifre", "Kullanıcı Adı veya Şifre Hatalı !");
+            }
+
+
+            return View();
+        }
+
+        public async Task<IActionResult> CikisYap()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("GirisYap");
+        }
+
+        public IActionResult Hata()
+        {
+            return View();
+        }
+
+
+        public IActionResult KayitOl()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult KayitOl(KayitViewModel hasta)
+        {
+            
+                        
+            if (ModelState.IsValid)
+            {
+                Hasta _hasta = new Hasta()
+                {
+                    Isim = hasta.Isim,
+                    Soyisim = hasta.Soyisim,
+                    TC = hasta.TC,
+                    Cinsiyet = hasta.Cinsiyet,
+                    Password = hasta.Password,
+                    Telefon = hasta.Telefon,
+                    RolId = 3
+                };
+                _context.Hastalar.Add(_hasta);
+                _context.SaveChanges();
+
+            }
+            return View(hasta);
+        }
+
+
+    }
+}
